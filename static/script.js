3b8e5edd71fdde625b5ac1f099379835d94b1939
Giovanni <indirizzoemail@mail.com>
2020-11-18 01:05:02 +0100

    add floating emojis based on temperature

--- static/script.js
+++ static/script.js
@@ -23,14 +23,69 @@ const parseTemperature = () => {
 
 const setBackgroundColorFromTemperature = () => {
     const temperature = parseTemperature()
-    const backgroundPositionPercentage = proportion(temperature, 23, 30, 0, 100)
+    const backgroundPositionPercentage = proportion(temperature, 23, 32, 0, 100)
     document.getElementsByClassName("rainbow")[0].style.backgroundPosition = `${backgroundPositionPercentage}%`
 }
 
+let lastEmoji = null
+
+const getCurrentEmojiFromTemperature = () => {
+    currentEmoji = parseTemperature() > 26.0 ? 'fire.png' : 'cold.png'
+
+    if (lastEmoji === null) {
+        lastEmoji = currentEmoji
+    }
+
+    if (currentEmoji !== lastEmoji) {
+        // Emoji change detected, update all existing emojis
+        for (let emoji in document.getElementsByClassName("emoji")) {
+            console.log("updating emojis")
+            emoji["src"] = currentEmoji
+        }
+
+        lastEmoji = currentEmoji
+    }
+
+    return currentEmoji
+}
+
+function generatePosition() {
+    const x = Math.round((Math.random() * 100) - 10) + '%';
+    const y = Math.round(Math.random() * 100) + '%';
+    return [x, y];
+}
+
+function generateEmojis() {
+    const emoji = document.createElement("img")
+    const position = generatePosition()
+
+    emoji.classList.add("emoji")
+
+    emoji["src"] = getCurrentEmojiFromTemperature()
+    emoji.style.top = position[0]
+    emoji.style.left = position[1]
+
+    document.body.appendChild(emoji)
+
+    setTimeout(() => {
+        emoji.style.opacity = '1'
+        emoji.classList.add('generated')
+    }, 100)
+
+    setTimeout(() => {
+        emoji.parentElement.removeChild(emoji)
+    }, 2000)
+}
+
+
 (function() {
-    const reloadInterval = 1000;
+    const reloadInterval = 1000
+    const emojiInterval = 400
 
     setBackgroundColorFromTemperature()
+
+    setInterval(generateEmojis, emojiInterval);
+    generateEmojis();
 
     setInterval(async() => {
         await refreshPageContent()
